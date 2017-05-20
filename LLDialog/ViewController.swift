9cc94ea8ac215d60eb427efa03c9c87c52457120
ApolloZhu <public-apollonian@outlook.com>
2017-05-20 13:24:06 -0400

    Ver 1.1
    - Use UIView.animate completion instead of timer to support older targets
    - Support reusing the dialog object
    - Support showing in a custom parent view
    - Support use in extension targets, kind of solve #5 after almost a year
    - Update readme

--- LLDialog/ViewController.swift
+++ LLDialog/ViewController.swift
@@ -11,28 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
-    
+
     @objc private func tappedPositiveButton() {
-        
         label.text = "Accepted"
     }
-    
+
     @objc private func tappedNegativeButton() {
-        
         label.text = "Did not accept"
     }
-    
+
     @IBAction func buttonTapped() {
-        
+
         let dialog = LLDialog()
-        
+
+        // Set title. (Not required)
         dialog.title = "Use Google's location service?"
-        
+
+        // Set message. (Not required)
         dialog.message = "Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running."
-        
+
+        // Set the buttons.
+        // Title for positive button is required. If set it to be a "blank" string, it will automatically change to "OK"
         dialog.setPositiveButton(withTitle: "AGREE", target: self, action: #selector(tappedPositiveButton))
-        dialog.setNegativeButton(withTitle: "DISAGREE", target: self,  action: #selector(tappedNegativeButton))
-        
+        dialog.setNegativeButton(withTitle: "DISAGREE", target: self, action: #selector(tappedNegativeButton))
+
+        // At last, show the dialog.
         dialog.show()
         
     }
